ba4248459e6d4626e29889bcccf7516ca322647a
jones21312321313213 <skinnyjones212121@gmail.com>
2025-11-11 18:15:02 +0800

    fixed some things

--- contacts.py
+++ contacts.py
@@ -2,8 +2,8 @@ import streamlit as st
 import re
 import requests
 
+webhook_url = st.secrets["WEBHOOK_URL"]
 
-WEBHOOK_URL = "https://connect.pabbly.com/workflow/sendwebhookdata/IjU3NjYwNTY0MDYzMDA0MzE1MjY0NTUzNDUxMzQi_pc"
 
 
 def is_valid_email(email):
@@ -16,30 +16,30 @@ def contact():
     email = st.text_input("Email")
     message = st.text_area("Message")
     if st.button("Submit"):
-        if not WEBHOOK_URL:
+        if not webhook_url:
             st.error("Email servie is not set up.Please try again later.")
-            st.stop()
+            return
         
         if not name:
             st.error("Please provide your name.")
-            st.stop()
+            return
             
         if not email:
             st.error("Please provide your email address.")
-            st.stop()
+            return
             
         if not is_valid_email(email):
             st.error("Please provide a valid email address.")
-            st.stop()
+            return
             
         if not message:
             st.error("Please provide a message.")
-            st.stop()
+            return
             
         
         #Prepare the data payload and send it to the specified webhook URL
         data = {"email":email, "name":name, "message":message}
-        response = requests.post(WEBHOOK_URL,json = data)
+        response = requests.post(webhook_url,json = data)
         
         if response.status_code == 200:
             st.success("Your message has been sent successfully! ")
